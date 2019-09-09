5e7a0f254bf287b55a3d086d0134aa5aa3a358da
KEINOS <github@keinos.com>
2019-09-09 14:15:33 +0900

    refactor: use variables for file names and paths

--- install-composer.sh
+++ install-composer.sh
@@ -6,34 +6,42 @@ echo '=============================='
 echo ' Installing PHP Composer'
 echo '=============================='
 
-# Verify hash value of the signature file
-EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+path_file_setup_composer='/composer-setup.php'
+path_dir_current=$(pwd)
 
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
-then
+# Clean Up files on exit
+function cleanUpFiles(){
+    echo "- Removing file: ${path_file_setup_composer}"
+    rm -f ${path_file_setup_composer:-/composer-setup.php}
+}
+trap cleanUPFiles EXIT INT QUIT KILL TERM HUP
+
+# Verify hash value of the signature file
+echo -n '- Verifing installer signature: '
+EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', '${path_file_setup_composer}');"
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', '${path_file_setup_composer}');")"
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]; then
     >&2 echo 'ERROR: Invalid installer signature'
-    rm composer-setup.php
     exit 1
 fi
+echo 'OK'
 
 echo '- Setup composer:'
-php composer-setup.php
+php $path_file_setup_composer
 if [ $? -ne 0 ]; then
     echo 'ERROR: Can not install composer.'
-    rm composer-setup.php
     exit 1
 fi
 
-echo '- Moving composer to env path:'
+echo -n '- Moving composer to env path: '
 mv composer.phar /usr/bin/composer && \
 composer --version
 if [ $? -ne 0 ]; then
     echo 'ERROR: Can not run composer.'
-    rm composer-setup.php
     exit 1
 fi
+echo 'OK'
 
 echo '=============================='
 echo ' Running Composer diagnose'
@@ -43,12 +51,17 @@ composer diagnose
 echo '=============================='
 echo ' Running Hello-World for test'
 echo '=============================='
-mkdir ~/sample && \
-cd ~/sample && \
+
+name_file_sample='sample.php'
+path_dir_sample="${path_dir_current}/sample"
+path_file_sample="${path_dir_sample}/${name_file_sample}"
+
+mkdir $path_dir_sample && \
+cd $path_dir_sample && \
 composer init --quiet --name sample/hello-world --require rivsen/hello-world:dev-master && \
 composer install
 
-cat << 'SCRIPTSOURCE' > sample.php
+cat << 'SCRIPTSOURCE' > $path_file_sample
 <?php
 require_once "vendor/autoload.php";
 
@@ -58,6 +71,5 @@ exit(0);
 
 SCRIPTSOURCE
 
-php sample.php && \
-rm -rf ~/sample && \
-rm /composer-setup.php
+php $path_file_sample && \
+rm -rf $path_file_sample
